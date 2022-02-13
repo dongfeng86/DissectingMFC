b2138bb5298c754122d76b2b88a575ddd653e977
dongfeng86 <115949168@qq.com>
2022-02-13 22:47:03 +0800

    2022.2.13 家中上传

---
+++ 函数指针的强制类型转换/函数指针的强制类型转换.cpp
@@ -0,0 +1,68 @@
+﻿// 这是神奇的一幕，我们知道，基类指针想要调用子类成员函数，一般需要将该函数定义
+//为虚函数，通过在类中维护虚函数表，从而实现基类指针调用子类方法的效果。但是虚函数
+//需要在每个类中维护一个虚函数表，大大增加了程序了空间复杂度。
+//在MFC中，没有这么做，而是将子类的所有相应函数统一转为基类的某个响应函数指针，然后
+//通过对比消息号来确定某个子类拥有合适的响应函数。这样做的关键是：通过成员解除引用
+//运算符，可以将基类指针指向子类成员函数。
+//这种处理方式的关键，是要理解成员解除引用运算符。基类指针使用成员解除引用运算符时，
+//实际上流程会转到相应的子类函数中，基类指针（实际指向子类对象）把this指针压栈，
+//子类成员函数根据这个地址可以正确找到相应的类成员。
+
+
+#include<iostream>
+using namespace std;
+
+class CBase
+{
+public:
+	void BasePrintMsg() {
+		cout << "In Base class" << endl;
+	}
+};
+
+class CDerive :public CBase
+{
+public:
+	CDerive() :m_iDerive(3) {}
+	int GetInt(int m) {
+		return m_iDerive * m;
+	}
+
+	double GetDouble(int m, double d) {
+		return m * d;
+	}
+private:
+	int m_iDerive;
+};
+
+typedef void (CBase::* pBaseVoidFun)();
+
+union UMapFuns
+{
+	//请注意，以下三个成员都是基类的成员函数的函数指针
+	pBaseVoidFun pfn;
+	double (CBase::*pfn_double)(int m, double d);
+	int (CBase::*pfn_int)(int m);
+};
+
+int main()
+{
+	UMapFuns uMapFun;
+	uMapFun.pfn = (pBaseVoidFun)&CDerive::GetInt;
+
+	CDerive cDeriveObj;
+	CBase* pBase = &cDeriveObj;
+	//调用成功，关键要理解uMapFun.pfn_int的地址实际上是子类成员函数的地址，子类对象
+	//调用解除引用运算符时，不过是将子类对象的地址压栈，让子类成员函数能够根据这个
+	//地址找到类的其他成员。
+	int i = (cDeriveObj.*(uMapFun.pfn_int))(3);	
+	//调用成功，用基类指针调用子类函数。关键要理解uMapFun.pfn_int的地址实际上是子类
+	//成员函数的地址，基类指针此时的地址就是子类对象的地址，该地址压栈后，
+	//子类成员函数根据这个地址找到的类的其他成员都是正确的。
+	int i2 = (pBase->*(uMapFun.pfn_int))(4);	
+
+	uMapFun.pfn = (pBaseVoidFun)&CDerive::GetDouble;
+	double d = (pBase->*(uMapFun.pfn_double))(3, 4.0);	//调用成功，用基类指针调用子类函数
+
+	return 0;
+}
--- 强制类型转换/强制类型转换.cpp
+++ 强制类型转换/强制类型转换.cpp
@@ -1,4 +1,13 @@
-﻿//强制类型转换
+﻿//函数的强制类型转换
+/*
+本文的总体结论：
+1.强制类型转换type(expr)和static_cast并不完全相同，后者会检查参数类型和目标类型是否一致。
+将某个函数指针赋给另一函数指针时，如果两者签名不同，type(expr)可以赋值，但是static_cast却无法赋值的。
+2.在单继承类中，可以通过强制类型转换type(expr)将类成员函数赋给继承链中的任意成员函数指针，甚至
+可以赋给其他类函数指针；而static_cast因为会进行函数签名检查，类型不同的无法转换。
+3.在多继承中，若子类方法参数与返回值和父类相同，且没有二义性，则可将子类函数地址赋给相应父类方法指针；
+同一类层次中，如果两个函数签名相同，则可以相互转换。不可将多继承中的类成员函数直接赋值给一个不相关的类函数指针
+*/
 
 #include <iostream>
 #include <tchar.h>
@@ -7,89 +16,54 @@ using namespace std;
 class CBase
 {
 public:
-	virtual void PrintMsg() {
-		cout << "\nPrint in CBase class!\n";
-	}
-	int GetInteger(int i) {
-		return i;
-	}
+	virtual void PrintMsg() { cout << "\nPrint in CBase class!\n"; }
+	int GetInteger(int i) { return i; }
 };
 
 class CA :public CBase
 {
 public:
-	virtual void PrintMsg() {
-		cout << "\nPrint in CADerive class!\n";
-	}
-
-	int GetInteger(int i){
-		return i + 1;
-	}
+	virtual void PrintMsg() { cout << "\nPrint in CADerive class!\n"; }
+	int GetInteger(int i) { return i + 1; }
 };
 
 class CB :public CBase
 {
 public:
-	virtual void PrintMsg() {
-		cout << "\nPrint in CBDerive class!\n";
-	}
+	virtual void PrintMsg() { cout << "\nPrint in CBDerive class!\n"; }
 };
 
 class CSI :public CA
 {
 public:
-	virtual void PrintMsg() {
-		cout << "\nPrint in CSIDerive class!\n";
-	}
-
-	int GetInteger(int i){
-		return i+1;
-	}
+	virtual void PrintMsg() { cout << "\nPrint in CSIDerive class!\n"; }
+	int GetInteger(int i) { return i + 1; }
 };
 
 class CMI :public CA, public CB
 {
 public:
-	void PrintMsg() {
-		cout << "\nPrint in CSIDerive class!\n";
-	}
-	int GetInteger(int i){
-		return 8;
-	}
-
-	int GetIntegerDouble(int i, double m)
-	{
-		return i * m;
-	}
-
-	void Hello()	{
-		cout << "\nPrint in CSIDerive class!\n";
-	}
+	void PrintMsg() { cout << "\nPrint in CSIDerive class!\n"; }
+	int GetInteger(int i) { return 8; }
+	double GetDouble(int i, double m) { return i * m; }
+	void Hello() { cout << "\nPrint in CSIDerive class!\n"; }
 };
 
 class COther
 {
 public:
-	virtual void PrintMsg() {
-		cout << "\nPrint in CSIDerive class!\n";
-	}
+	virtual void PrintMsg() { cout << "\nPrint in CSIDerive class!\n"; }
 };
 
 class CMI2 :public COther, public CA
 {
 public:
-	void NewPrint() {
-		cout << "\nPrint in CSIDerive class!\n";
-	}
+	void NewPrint() { cout << "\nPrint in CSIDerive class!\n"; }
 };
 
-void GlobalFunVoid(){
-	cout << "hello";
-}
+void GlobalFunVoid() { cout << "hello"; }
 
-int GlobalFunInt(int i) {
-	return i;
-}
+int GlobalFunInt(int i) { return i; }
 
 int main()
 {
@@ -114,61 +88,65 @@ int main()
 	pFunbaseVoid = (PFunBaseVoid)aDeriveFun;
 	(base.*pFunbaseVoid)();
 
-	//****************************************************************
-	//情况1：单继承，将子类某个成员函数指针强制转为祖父类某个成员函数指针
-	//1.1 在单继承中，可以将子类的成员函数强制转换为祖父类某个函数指针，函数参数或者返回值相同
-	pFunbaseVoid = (PFunBaseVoid)&CSI::PrintMsg;			//单继承这个写法没有问题
-	pFunbaseVoid = (PFunBaseVoid)(PFunAVoid)&CSI::PrintMsg;	//这种写法更严格
-	//1.2 在单继承中，可以将子类的成员函数强制转换为祖父类某个函数指针，哪怕函数参数或者返回值不同
-	pFunbaseVoid = (PFunBaseVoid)&CSI::GetInteger;	//这种是可以转换的
+	//第一部分****************************************************************
+	//1：单继承
+	//1.1 在单继承中，可以将子类的成员函数强制转换为祖父类某个函数指针，无论函数参数或者返回值是否相同
+	pFunbaseVoid = (PFunBaseVoid)&CSI::PrintMsg;			//ok，函数参数一致
+	pFunbaseVoid = (PFunBaseVoid)(PFunAVoid)&CSI::PrintMsg;	//ok,更严格
+	pFunbaseVoid = (PFunBaseVoid)&CSI::GetInteger;			//ok，函数参数不同
+	//pFunbaseVoid = static_cast<PFunBaseVoid>(&CSI::GetInteger);//编译错误。这是一个极为微妙的变化，static_cast会检查两个函数参数和返回类型是否相同
 
-	//****************************************************************
-	//情况2：多重继承
-	//2.1 多重继承中，将具有相同函数参数和返回值的子类函数指针强制转换为父类函数指针
-	//下面的转换涉及到一个非常隐蔽的知识点，在多重继承中，可能会有两个直接父类
-	//从同一基类派生出来，例如CBase→CA、CB→CMI,因此，当采用强制类型转换(BaseFun)miDeriveFun时
-	//编译器不知道是该转为CA这个类对应的BaseFun还是CB这个类对应的BaseFun，因此需要指定
+	//1.2 在单继承中，可以将类中某个成员函数赋给另一成员函数指针,哪怕函数参数或者返回类型不同
+	PFunSIVoid pSIVoidFun = (PFunSIVoid)&CSI::GetInteger;	//ok，函数参数不同
+
+	//第二部分****************************************************************
+	//2：多重继承
+	//2.1 多重继承中，若子类方法参数与返回值和直接父类相同，且没有二义性，则可将子类函数地址赋给相应父类方法指针
+	PFunAInt pFunAInt = (PFunAInt)&CMI::GetInteger;		//多重继承的类，如果子类函数参数和直接父类相同，可以转为直接父类函数指针
+	pFunAInt = static_cast<PFunAInt>(&CMI::GetInteger);	//另一种写法
+
+	//2.2 多重继承中，若子类方法参数与返回值和直接父类不同，不可将子类函数地址赋给相应直接父类方法指针
+	//PFunAInt = (PFunAInt)&CMI::PrintMsg;				//编译错误，如果方法参数不同，无法转换
+	//PFunAInt = static_cast<PFunAInt>(&CMI::PrintMsg);	//编译错误，如果方法参数不同，无法转换
+
+	//2.3 多重继承中，若子类方法参数与返回值和非直接父类相同，但有二义性，不可将子类函数地址赋给相应父类方法指针
+	//在多重继承中，可能会有两个直接父类从同一基类派生出来，例如CBase→CA、CB→CMI,因此，
+	//当采用强制类型转换(PFunBaseVoid)pFunMIVoidFun时,编译器不知道是该转为CA这个类对应的
+	//PFunBaseVoid还是CB这个类对应的PFunBaseVoid，因此需要指定
 	PFunMIVoid pFunMIVoidFun = &CMI::Hello;
-	//pFunbaseVoid = (PFunBaseVoid)pFunMIVoidFun;	//编译出错，出现二义性
-	pFunbaseVoid = (PFunBaseVoid)(PFunAVoid)pFunMIVoidFun;	//编译正确
+	//pFunbaseVoid = (PFunBaseVoid)pFunMIVoidFun;				//编译出错，出现二义性
+	//pFunbaseVoid =static_cast<PFunBaseVoid>(pFunMIVoidFun);	//编译出错，出现二义性
+	pFunbaseVoid = (PFunBaseVoid)(PFunAVoid)pFunMIVoidFun;		//ok，两次转换避免二义性
+	pFunbaseVoid = (PFunBaseVoid)&CMI2::NewPrint;		//ok，如果多重继承里面没有两个PFunBaseVoid类型函数
 
-	//这是一个反例，如果说多重继承里面没有两个BaseFun指针对象，一切ok
-	typedef void(CMI2::*PCMI2Fun)();
-	PCMI2Fun pCmi2Fun = &CMI2::NewPrint;
-	pFunbaseVoid = (PFunBaseVoid)pCmi2Fun;
+	//2.4 多重继承中，如果类内某两个函数参数或者返回类型不同，这两个函数无法相互转换
+	//PFunMIVoid pFunMIVoid = (pFunMIVoid)&CMI::GetInteger;
 
-	//2.2 多重继承中，如果子类函数参数和返回值与父类不同，无法强制转换
-	PFunMIInt pFunMIInt = &CMI::GetInteger;
-	//PFunMIVoid pFunMIVoid = (pFunMIVoid)pFunMIInt;//多重继承的类，当函数签名不同时，无法将一个函数指针强制转为另一个
-	//pFunbaseVoid = (PFunBaseVoid)pFunMIInt;		//多重继承的类，如果子类函数参数和父类不同，无法强制转换
-	//pFunbaseVoid = static_cast<PFunBaseVoid>(pFunMIInt);	//上面代码的另一种写法，static_cast可以进行函数参数匹配检查
-	PFunAInt pFunAInt = (PFunAInt)pFunMIInt;		//多重继承的类，如果子类函数参数和父类相同，可以转为父类函数指针
-	PFunAInt aaaaInt = static_cast<PFunAInt>(pFunMIInt);//另一种写法
+	//2.5 在多重继承中，若子类函数参数与返回值和祖父类不同，一般需要经过两次转换
+	//CMI::GetInteger和PFunAInt参数返回值相同，且CMI和CA为直接继承，所以(PFunAInt)&CMI::GetInteger成功转换，
+	//而CA与CBase为单继承，因此PFunAInt指针可以直接转为PFunBaseVoid指针
+	pFunbaseVoid = (PFunBaseVoid)(PFunAInt)&CMI::GetInteger;					//ok
+	pFunbaseVoid = (PFunBaseVoid)(static_cast<PFunAInt>(&CMI::GetInteger));//另一种写法
 
-	//PFunBaseInt pFunBaseInt = (PFunBaseInt)(pFunMIInt);	//出现二义性
-	//PFunBaseInt pFunBaseInt = static_cast<PFunBaseInt>(pFunMIInt);	//出现二义性
+	//第三部分****************************************************************
+	//3：将一个类的成员函数赋给另外一个类的成员函数指针
+	//3.1 单继承的类方法赋给另一个单继承的类方法指针，哪怕函数参数不同也可以
+	PFunOther otherFun1 = (PFunOther)&CBase::PrintMsg;	//ok
+	otherFun1 = (PFunOther)&CSI::GetInteger;			//ok
+	PFunSIInt pSIGetIntFun = (PFunSIInt)&CBase::PrintMsg;	//ok
 
+	otherFun1 = (PFunOther)&CSI::PrintMsg;//ok
+	//otherFun1 = static_cast<PFunOther>(&CSI::PrintMsg);//编译错误。static_cast发现两个方法不属于同一类，且两个类没有继承关系
 
-	//PFunAInt bInt = static_cast<PFunAInt>(&CMI::GetIntegerDouble);//报错
-	//PFunAInt bInt = (PFunAInt)(&CMI::GetIntegerDouble);//报错,做了函数签名检查
+	//3.2 多继承中，哪怕方法参数一致，也无法将多继承类方法赋给不相关的令一个类方法指针
+	//otherFun1 = (PFunOther)&CMI::PrintMsg;	//编译错误，在多重继承的类方法中，无法这样赋值
+	//otherFun1 = (PFunOther)&CMI::GetInteger;				//编译错误
+	//otherFun1 = static_cast<PFunOther>(&CMI::GetInteger);	//编译错误
 
-	pFunbaseVoid = (PFunBaseVoid)(PFunAInt)pFunMIInt;	//由于CA为单继承，pFunMIInt和pFunAInt参数返回值相同，因此编译通过
-	PFunBaseVoid bbbb = (PFunBaseVoid)(static_cast<PFunAInt>(pFunMIInt));	//static_cast可以进行函数参数检查
-
-	//****************************************************************
-	//情况3：将一个类的成员函数付给另外一个类的成员函数指针,哪怕函数参数不同也可以
-	PFunOther otherFun1 = (PFunOther)&CBase::PrintMsg;
-	PFunOther otherFun2 = (PFunOther)&CSI::GetInteger;
-	PFunSIInt pSIGetIntFun= (PFunSIInt)&CBase::PrintMsg;
-
-	//情况4：全局函数指针可以互转（即便函数参数不同）
+	//第四部分****************************************************************
+	//4：全局函数指针可以互转（即便函数参数不同）
 	PGlobalFunVoid pFunVOid = (PGlobalFunVoid)&GlobalFunInt;	//参数不同的全局函数指针可以互相赋值
+	//pFunVOid = static_cast<PGlobalFunVoid>(&GlobalFunInt);	//编译错误，static_cast会检查参数是否相同
 	PGlobalFunInt pFunInt = (PGlobalFunInt)&GlobalFunVoid;
-
-	//情况5：一些胡乱转换
-	//这种是没有办法转换的，自己一直以为强制类型转换，仅仅是直接赋值，实际上，
-	//这种想法是错误的，编译器进行了类型检查，会对一些无效的转换进行报错。
-	//PGlobalFunVoid pFun = (PGlobalFunVoid)&CBase::PrintMsg;	//这个出错的原因是：成员函数拥有一个隐藏的参数this指针
-	CSI siDerive;
-	//double mm = (double)siDerive;//这是不会成功的，因为CSI没有定义到double的转换函数
+	//PGlobalFunVoid pFun = (PGlobalFunVoid)&CBase::PrintMsg;	//编译出错：成员函数拥有一个隐藏的参数this指针，强制类型转换会检查这个
 }
